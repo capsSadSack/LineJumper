4e0e1787563c40e7c20a28fa9454eaaac028f0f3
capsSadSack <cherkashin@phystech.edu>
2020-06-06 10:16:44 +0300

    MainMenu scene was created (the simplest boring design)

---
+++ Assets/Assets/Scripts/ForScenes/MainMenuController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void LoadPlayScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LoadHowToPlayScene()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LoadRecordsScene()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LoadAchievementsScene()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
