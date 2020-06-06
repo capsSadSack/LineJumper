a9007da3301ba30736c593978be169af7cb141d4
capsSadSack <cherkashin@phystech.edu>
2020-06-06 13:35:43 +0300

    MenuButtonScene: Animations were added

---
+++ Assets/Assets/Scripts/Interface/MenuButtonBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class MenuButtonBehaviour : MonoBehaviour
+{
+    public Animator anim;
+
+    public void OnMouseEnter()
+    {
+        anim.SetBool("isSelected", true);
+    }
+
+    public void OnMouseExit()
+    {
+        anim.SetBool("isSelected", false);
+        anim.SetBool("isPressed", false);
+    }
+
+    public void OnMouseDown()
+    {
+        anim.SetBool("isPressed", true);
+    }
+}
